0de9c321944bd851480b172a61dfdd07669f4ed9
SP7-creator <76457031+SP7-creator@users.noreply.github.com>
2021-03-04 19:00:16 -0500

    Add files via upload

---
+++ Monster.js
@@ -0,0 +1,25 @@
+class Monster {
+  constructor(x,y,r){
+var options = { 
+    density : 5,
+	friction : 0,
+    };
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.image = loadImage("Monster-01.png");
+    this.body = Bodies.circle(this.x,this.y,(this.r)/ 2,options);
+	World.add(world.this.body);
+	}
+	display()
+	{
+	var pos = this.body.position
+	push()
+	translate(pos.x,pos.y - 100)
+	fill("orange");		
+	imageMode(CENTER);
+	image(this.image,0,0,this.r,this.r)		
+	pop()		
+			
+	}
+}
